c144b11ec2f88db518aeac819ff3a9655f0c7a67
dickson anyaele <dicksonanyaele1234@gmail.com>
2023-07-09 02:38:18 -1200

    fixed json parser and rest...

--- view/src/core/Translator.js
+++ view/src/core/Translator.js
@@ -1,94 +1,71 @@
 export class Translator {
-  constructor(object) {
-    this.jsStringObject = object;
-    this.jsType = null;
-    this.variableName = '';
-  }
-
-  capitalizeFirstLetter(str) {
-    if (typeof str !== 'string') return '';
-
-    if (str.length === 0) return str;
-
-    const firstLetter = str.charAt(0).toUpperCase();
-    const restOfLetters = str.slice(1).toLowerCase();
-
-    return firstLetter + restOfLetters;
-  }
-
-  preprocess() {
-    // convert or single quote to double qoute
-    var convertedStr = this.jsStringObject.replace(/'/g, '"');
-    return convertedStr;
-  }
-
-  getVariableName() {
-    return this.variableName;
-  }
-
-  define() {
-    const getObject = this.preprocess().split('=');
-    const getObjectName = getObject[0].split(' ')[1];
-    const getObjType = getObject[1].trim();
-
-    this.jsType = JSON.parse(getObjType);
-    this.variableName = getObjectName + 'Type';
-  }
-
-  translatePrimitiveType(object) {
-    if (typeof object === 'object' || Array.isArray(object)) {
-      return false;
+    constructor(object) {
+        this.jsType = object;
+        this.variableName = '';
     }
 
-    return typeof object;
-  }
-
-  translator(object = this.jsType) {
-    if (Array.isArray(object)) {
-      for (let i = 0; i < object.length; i++) {
-        let isPrimitive = this.translatePrimitiveType(object[i]);
-
-        if (isPrimitive !== false) {
-          object[i] = isPrimitive;
-        } else {
-          this.translator(object[i]);
+    translatePrimitiveType(object) {
+        if (typeof object === 'object' || Array.isArray(object)) {
+            return false;
         }
-      }
-    } else if (typeof object === 'object') {
-      for (var key in object) {
-        let isPrimitive = this.translatePrimitiveType(object[key]);
 
-        if (isPrimitive !== false) {
-          object[key] = isPrimitive;
-        } else {
-          this.translator(object[key]);
-        }
-      }
+        return typeof object;
     }
 
-    return object;
-  }
+    translator(object = this.jsType) {
+        if (Array.isArray(object)) {
+            for (let i = 0; i < object.length; i++) {
+                let isPrimitive = this.translatePrimitiveType(object[i]);
 
-  refine(obj) {
-    for (var key in obj) {
-      if (Array.isArray(obj[key])) {
-        let arrSet = new Set(obj[key]);
-        if (arrSet.size === 1) {
-          obj[key] = `${obj[key][0]}[]`;
-        } else if (arrSet.size === 2) {
-          const getArrSet = Array.from(arrSet);
-          let str = `${getArrSet[0]} | ${getArrSet[1]}[]`;
-          obj[key] = str;
+                if (isPrimitive !== false) {
+                    object[i] = isPrimitive;
+                } else {
+                    this.translator(object[i]);
+                }
+            }
+        } else if (typeof object === 'object') {
+            for (var key in object) {
+                let isPrimitive = this.translatePrimitiveType(object[key]);
+
+                if (isPrimitive !== false) {
+                    object[key] = isPrimitive;
+                } else {
+                    this.translator(object[key]);
+                }
+            }
         }
-      } else if (typeof obj[key] === 'object') {
-        this.refine(obj[key]);
-      }
-    }
-  }
 
-  print() {
-    console.log(`type ${this.variableName} = ${this.translator()}`);
-  }
+        return object;
+    }
+
+
+    refine(obj) {
+        if (typeof obj === 'object') {
+            for (var key in obj) {
+                if (Array.isArray(obj[key])) {
+                    let arrSet = new Set(obj[key]);
+
+                    if (arrSet.size === 1) {
+                        obj[key] = `${
+                            obj[key][0]
+                        }[]`;
+                        // } else if (arrSet.size === 2) {
+                        //     const getArrSet = Array.from(arrSet);
+                        //     let str = `${
+                        //         getArrSet[0]
+                        //     } | ${
+                        //         getArrSet[1]
+                        //     }[]`;
+                        //     obj[key] = str;
+                        // } else {
+                        return obj[key]
+                    }
+                } else if (typeof obj[key] === 'object') {
+                    this.refine(obj[key]);
+                }
+            }
+        }
+    }
 }
 
 // const translate = new Translator(`const bird = ["dickson", 1, 2,{ "key2": { "nestedKey1": "nestedValue1", "nestedKey2": { "deepKey1": "deepValue1","deepKey2": "deepValue2"}}},{ "key": "value", "key1": { "keySub1": 1 }}]`);
--- view/src/index.js
+++ view/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { InputProvider } from './context/input';
-import { OutputProvider } from './context/output';
+import {InputProvider} from './context/input';
+import {OutputProvider} from './context/output';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <InputProvider>
-      <OutputProvider>
-        <App />
-      </OutputProvider>
-    </InputProvider>
-  </React.StrictMode>
+root.render (
+    <React.StrictMode>
+        <InputProvider>
+            <OutputProvider>
+                <App/>
+            </OutputProvider>
+        </InputProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
